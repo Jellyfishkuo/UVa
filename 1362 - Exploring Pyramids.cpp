1d19b75a6e570f25a0b901673b84c0d42ac43d0e
Jellyfishkuo <67105816+Jellyfishkuo@users.noreply.github.com>
2023-05-24 02:45:19 +0800

    Create 1362 - Exploring Pyramids.cpp

---
+++ 1362 - Exploring Pyramids.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define maxn 310
+#define int long long
+using namespace std;
+
+char s[maxn];
+long long d[maxn][maxn];
+long long dp(int i,int j){
+    if(d[i][j]!=-1) return d[i][j];
+    if(i==j) return 1;
+    if(s[i]!=s[j]) return 0;
+    long long& ans=d[i][j];
+    ans=0;
+    for(int k=i+2;k<=j;k++)
+        ans=(ans+dp(i+1,k-1)*dp(k,j))%1000000000;
+    return ans;
+}
+
+main(){
+    while(cin>>s){
+        memset(d,-1,sizeof(d));
+        cout<<dp(0,strlen(s)-1)<<endl;
+    }
+    return 0;
+}
