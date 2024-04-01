8a73d0be29f9a0080af0c84b0e08338e935c20dd
Jellyfishkuo <67105816+Jellyfishkuo@users.noreply.github.com>
2024-04-01 13:52:18 +0800

    Update 1312 - Cricket Field.cpp

--- 1312 - Cricket Field.cpp
+++ 1312 - Cricket Field.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cas,n,w,h,y[105],len;
+int cas,n,w,h,line[105],len;
 
 struct point{
     int x,y;
 };
 
-bool cmp(point a,point b) {
+bool cmp(point a,point b){
     return a.x<b.x||(a.x==b.x&&a.y<b.y);
 }
 
@@ -15,29 +15,33 @@ point p[105];
 
 void solve(){
     sort(p,p+n,cmp);
-    sort(y,y+n+2);
-    len=unique(y,y+n+2)-y;
+    sort(line,line+n+2);
+    len=unique(line,line+n+2)-line;
     int ansx,ansy,ans=-1,hh,ww;
     for(int i=0;i<len;i++){
         for(int j=i+1;j<len;j++){
-            hh=y[j]-y[i];
+            hh=line[j]-line[i];
             int nowx=0;
             for(int k=0;k<n;k++){
-                if(p[k].y<=y[i]||p[k].y>=y[j]) continue;
-                ww=p[k].x-nowx;
-                ww=min(ww,hh);
-                if(ans<ww){
+                if(p[k].y<=line[i]||p[k].y>=line[j]) continue;
+                ww=p[k].x-nowx;  // ww: 目前小區域矩形的寬度
+                ww=min(ww,hh);   // ww: 設定為小區域內最大正方形的邊長
+
+                if(ans<ww){      // 如果目前的正方形邊長(ww)最大
                     ansx=nowx;
-                    ansy=y[i];
-                    ans=ww;
+                    ansy=line[i];
+                    ans=ww;      // 則更新正方形位置(ansx,ansy)與邊長ans
+
                 }
-                nowx=p[k].x;
+                nowx=p[k].x;     // 設定下一條垂直掃描線為x=p[k].x
+
             }
             ww=w-nowx;
-            ww=min(ww,hh);
+            ww=min(ww,hh);       // 處理掃描區間x=nowx至x=w
+
             if(ans<ww){
                 ansx=nowx;
-                ansy=y[i];
+                ansy=line[i];
                 ans=ww;
             }
         }
@@ -52,10 +56,10 @@ int main(){
         cin>>n>>w>>h;
         for(int i=0;i<n;i++){
             cin>>p[i].x>>p[i].y;
-            y[i]=p[i].y;
+            line[i]=p[i].y;
         }
-        y[n]=0;
-        y[n+1]=h;
+        line[n]=0;
+        line[n+1]=h;
         solve();
     }
     return 0;
