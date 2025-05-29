9eb1bf93fcd652b6dc90e2dea962a8f831104da4
Jellyfishkuo <67105816+Jellyfishkuo@users.noreply.github.com>
2025-05-29 16:39:47 +0800

    Update 1378 - A Funny Stone Game.cpp

--- 1378 - A Funny Stone Game.cpp
+++ 1378 - A Funny Stone Game.cpp
@@ -1,50 +1,74 @@
 #include <bits/stdc++.h>
-#define maxn 23+5
+#define maxn 30
 using namespace std;
 
 int SG[maxn];
-int visited[1000+5];
+int vis[1005];
 int pile[maxn],ans;
 
-void calculateSG(){
+void CalculateSG()
+{
     SG[0]=0;
-    for(int i=1;i<=maxn;i++){
+    for(int i=1;i<=maxn;i++)
+    {
         int cur=0;
         for(int j=0;j<i;j++)
             for(int k=0;k<=j;k++)
-                visited[SG[j]^SG[k]]=i;
-        while(visited[cur]==i) cur++;
+            vis[SG[j]^SG[k]]=i;
+        while(vis[cur]==i) cur++;
         SG[i]=cur;
     }
 }
 
-int main(){
-    calculateSG();
-    int Case=0,n;
-    while(cin>>n,n){
+int main()
+{
+    CalculateSG();
+    int cas=0,n;
+    while(cin>>n&&n)
+    {
         ans=0;
         for(int i=1;i<=n;i++) cin>>pile[i];
-        for(int i=1;i<=n;i++) if(pile[i]&1) ans^=SG[n-i];
-        cout<<"Game "<<++Case<<": ";
+        for(int i=1;i<=n;i++)
+            if(pile[i]&1)
+                ans^=SG[n-i];
+        cout<<"Game "<<++cas<<": ";
         if(!ans) cout<<"-1 -1 -1\n";
-        else{
-            bool flag=0;
-            for(int i=1;i<=n;i++){
-                if(pile[i]){
-                    for(int j=i+1;j<=n;j++){
-                        for(int k=j;k<=n;k++){
-                            if((SG[n-i]^SG[n-j]^SG[n-k])==ans){
+        else
+        {
+            bool f=0;
+            for(int i=1;i<=n;i++)
+            {
+                if(pile[i])
+                {
+                    for(int j=i+1;j<=n;j++)
+                    {
+                        for(int k=j;k<=n;k++)
+                        {
+                            if((SG[n-i]^SG[n-j]^SG[n-k])==ans)
+                            {
                                 cout<<i-1<<" "<<j-1<<" "<<k-1<<endl;
-                                flag=1;
+                                f=1;
                                 break;
                             }
                         }
-                        if(flag) break;
+                        if(f) break;
                     }
-                    if(flag) break;
+                    if(f) break;
                 }
             }
         }
     }
     return 0;
 }
+
+
+/*
+4
+1 0 1 100
+3
+1 0 5
+2
+2 1
+0
+
+*/
