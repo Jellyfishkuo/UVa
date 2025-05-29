3d9b50b702076bd16bffba72958e1bb3551bd897
Jellyfishkuo <67105816+Jellyfishkuo@users.noreply.github.com>
2025-05-29 18:43:24 +0800

    Update 10152 - ShellSort.cpp

--- 10152 - ShellSort.cpp
+++ 10152 - ShellSort.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t,n;
-    string s;
+int t,n;
+string s;
+    
+int main() 
+{
     cin>>t;
-    while(t--){
+    while(t--)
+    {
         cin>>n;
-        getline(cin, s);
+        getline(cin,s);
         string target[n],source[n];
         for(int i=0;i<n;i++) getline(cin,source[i]);
         for(int i=0;i<n;i++) getline(cin,target[i]);
         int pt=n-1,ps=n-1;
-        while(ps>=0){
+        while(ps>=0)
+        {
             if (source[ps]==target[pt]) pt--;
             ps--;
         }
