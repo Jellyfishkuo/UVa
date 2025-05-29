36a131cc365e944af2cedd0cb3d3cea7528923fe
Jellyfishkuo <67105816+Jellyfishkuo@users.noreply.github.com>
2025-05-29 12:52:15 +0800

    Update 1169 - Robotruck.cpp

--- 1169 - Robotruck.cpp
+++ 1169 - Robotruck.cpp
@@ -24,20 +24,20 @@ int main()
         for(int i=1;i<=n;i++)
         {
             cin>>x[i]>>y[i]>>w;
-            d2o[i]=abs(x[i])+abs(y[i]);
-            total_dist[i]=total_dist[i-1]+abs(x[i]-x[i-1])+abs(y[i]-y[i-1]);
-            total_weight[i]=total_weight[i-1]+w;
+            d2o[i]=abs(x[i])+abs(y[i]);                                        // 到原點距離
+            total_dist[i]=total_dist[i-1]+abs(x[i]-x[i-1])+abs(y[i]-y[i-1]);   // 總距離
+            total_weight[i]=total_weight[i-1]+w;                               // 總重量
         }
         deque<int> q;
         q.push_back(0);
         for(int i=1;i<=n;i++)
         {
-            while(!q.empty()&&(total_weight[i]-total_weight[q.front()]>c))
-                q.pop_front();
-            d[i]=f(q.front())+total_dist[i]+d2o[i];
-            while(!q.empty()&&(f(q.back())>=f(i)))
-                q.pop_back();
-            q.push_back(i);
+            while(!q.empty()&&(total_weight[i]-total_weight[q.front()]>c))     // 如果從 q.front() 到目前 i 的總重量 > 背包容量 c，就不能從那裡轉移來
+                q.pop_front();                                                 // 所以把它從佇列前面移除
+            d[i]=f(q.front())+total_dist[i]+d2o[i];                            // 在 j 時的最佳狀態，加上從 j+1 到 i 的距離與花費
+            while(!q.empty()&&(f(q.back())>=f(i)))                             // 保持佇列中 f(j) 單調遞增（或至少不會比 i 好）
+                q.pop_back();                                                  // 因為 f(i) 越小越好，所以把不如它的通通踢掉
+            q.push_back(i);                                                    // q.front() 永遠是「最小的 f(j)」
         }
         cout<<d[n]<<endl;
         if(cas) cout<<endl;
